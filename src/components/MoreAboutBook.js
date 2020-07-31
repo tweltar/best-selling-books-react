f3e3f0b726427499a517335ae02d9c38d9b83dad
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-01 05:39:39 +0630

    add more about book page

---
+++ src/components/MoreAboutBook.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MoreAboutBook.css';
+
+const MoreAboutBook = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MoreAboutBook;
