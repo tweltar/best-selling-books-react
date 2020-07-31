79dbbb8925600139d45c63600a10e2deb55879f6
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-01 04:17:02 +0630

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Content from './components/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <hr/>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
