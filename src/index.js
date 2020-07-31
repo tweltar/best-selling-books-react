a1e5c3aae47a4f513a30106f1b278ee2ea74b341
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-01 04:15:14 +0630

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import axios from 'axios';
+import * as serviceWorker from './serviceWorker';
+
+axios.defaults.baseURL=process.env.REACT_APP_END_POINT;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
