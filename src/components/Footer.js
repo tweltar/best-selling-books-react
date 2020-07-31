b7e3ed8c550b27f8c803cc913970c774dad1e9fb
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-01 05:39:06 +0630

    add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>This webpage is written using the open source API from the New York Times.</p>
+            <div>&copy;&nbsp;<p> 2020 Twel Tar</p></div>
+        </footer>
+    );
+};
+
+export default Footer;
