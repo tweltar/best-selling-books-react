9484f8d8b7c4e8d0b650c72d1e2d215ca2c7f6e7
Twel Tar <sulatttweltarbo16@gmail.com>
2020-08-02 17:08:35 +0630

    add search function

--- src/components/CategoryDetail.js
+++ src/components/CategoryDetail.js
@@ -11,6 +11,10 @@ const CategoryDetail = ({ categoryName, updated, listNameEncoded }) => {
     const [books, setBooks] = useState([]);
     const [book, setBook] = useState({});
     const [num, setNum] = useState('10');
+    const [searchValue, setSearchValue] = useState("");
+    const [publishedDate, setPublishedDate] = useState("");
+    const [previousPublishedDate, setPreviousPublishedDate] = useState("");
+    const [publishedDateDesc, setPublishedDateDesc] = useState("");
 
     const fetchBooks = async () => {
 
@@ -19,10 +23,30 @@ const CategoryDetail = ({ categoryName, updated, listNameEncoded }) => {
             const res = await Axios.get(link);
             setBooks(res.data.results.books);
             setNum(res.data.num_results);
+            setPublishedDate(res.data.results.published_date);
+            setPreviousPublishedDate(res.data.results.previous_published_date);
+            setPublishedDateDesc(res.data.results.published_date_description);
         } catch (error) {
             console.log(error);
         }
-    }
+    };
+
+    const searchByDate = async (e) => {
+        e.preventDefault();
+        
+        const link = appendApiKey(searchValue + "/" + listNameEncoded + ".json");
+        try {
+            const res = await Axios.get(link);
+            setBooks(res.data.results.books);
+            setNum(res.data.num_results);
+            setPublishedDate(res.data.results.published_date);
+            setPreviousPublishedDate(res.data.results.previous_published_date);
+            setPublishedDateDesc(res.data.results.published_date_description);
+        } catch (error) {
+            console.log(error);
+            alert("Invalid Date");
+        }
+    };
 
     useEffect(() => {
         fetchBooks();
@@ -37,8 +61,17 @@ const CategoryDetail = ({ categoryName, updated, listNameEncoded }) => {
                             <div>
                                 <p>Top {num} Best Selling Books of</p>
                                 <h2>{categoryName}<span> - Updated {updated}</span></h2>
+                                {
+                                    publishedDate && previousPublishedDate && <p style={{fontSize: '85%', marginTop: '3px'}}>Published Date: {previousPublishedDate} - {publishedDate}</p>
+                                }
+                                <p style={{fontSize: '80%', marginTop: '3px'}}>{
+                                    publishedDateDesc==="latest" ? "Showing latest results" : "Showing results for " + searchValue
+                                }</p>
                             </div>
-                            <input className="search-button" type="search" placeholder="Search by date...YYYY-MM-DD" />
+                            <form className="search" onSubmit={(e) => {searchByDate(e);}}>
+                                <input className="search-bar" type="date" placeholder="Search by date... yyyy-mm-dd" onChange={(e) => setSearchValue(e.target.value)} />
+                                <i className="fas fa-search search-button" onClick={(e) => searchByDate(e)}></i>
+                            </form>
                         </div>
                         <div className="books">
                         {
